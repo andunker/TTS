aa034568069fc91531213b882727c6d440e334ae
Fabio Mora <andunker@hotmail.com>
2023-08-14 00:35:27 -0500

    final demo

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
+import dotenv
+import os
 import requests
+import base64
+
+dotenv.load_dotenv()
 
 # Set up the API endpoint and parameters
 api_url = "https://texttospeech.googleapis.com/v1/text:synthesize"
-api_key = "YOUR_GOOGLE_API_KEY"  # Replace this with your actual API key
+
+# Replace this with your actual API key
+api_key = os.environ.get("API_KEY")
 
 headers = {
     "Content-Type": "application/json",
@@ -10,11 +17,12 @@ headers = {
 
 data = {
     "input": {
-        "text": "Hello, this is a sample text for TTS."
+        # Hello, this is a sample text for TTS.
+        "text": "Hola, este es un ejemplo de texto a voz."
     },
     "voice": {
-        "languageCode": "en-US",
-        "name": "en-US-Standard-A",
+        "languageCode": "es-US",  # en-US
+        "name": "es-US-Standard-A",  # en-US
     },
     "audioConfig": {
         "audioEncoding": "MP3",
@@ -30,11 +38,15 @@ response = requests.post(api_url, headers=headers, json=data, params=params)
 
 # Check if the request was successful
 if response.status_code == 200:
-    audio_content = response.json()["audioContent"]
-    
-    # Save the audio content to a file
-    with open("output.mp3", "wb") as f:
-        f.write(audio_content.encode("utf-8"))
+    audio_content = response.content
+
+    decoded_audio = base64.b64decode(audio_content)
+
+# Write the decoded audio content to an .mp3 file
+    output_file_path = "output.mp3"
+    with open(output_file_path, "wb") as output_file:
+        output_file.write(decoded_audio)
+
     print("Audio content saved as 'output.mp3'")
 else:
     print(f"Request failed with status code: {response.status_code}")
